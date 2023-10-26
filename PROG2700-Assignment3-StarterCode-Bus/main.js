6b7514a333f330fe1b440917ae82799723bfbbf1
adamnear <nearing14@hotmail.com>
2023-10-26 11:48:05 -0300

    Now add the icon to the map

--- PROG2700-Assignment3-StarterCode-Bus/main.js
+++ PROG2700-Assignment3-StarterCode-Bus/main.js
@@ -15,7 +15,7 @@
             iconUrl: './bus.png',
             iconSize: [32, 32]
         }),
-    })
+    }).addTo(map);
 
     L.marker([44.650690, -63.596537]).addTo(map)
         .bindPopup('This is a sample popup. You can put any html structure in this including extra bus data. You can also swap this icon out for a custom icon. A png file has been provided for you to use if you wish.')
