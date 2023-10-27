163148b3f6ed9e0385b1a9634d82b82ee97544c8
adamnear <nearing14@hotmail.com>
2023-10-27 12:12:02 -0300

    Fixing the bus to fetch the array data

--- PROG2700-Assignment3-StarterCode-Bus/main.js
+++ PROG2700-Assignment3-StarterCode-Bus/main.js
@@ -1,6 +1,7 @@
 // IIFE
 (() => {
 
+
     //create map in leaflet and tie it to the div called 'theMap'
     let map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
@@ -11,26 +12,58 @@
     //Fetch live bus data
     fetch('https://prog2700.onrender.com/hrmbuses')
         .then(response => response.json())
-        .then(data => {
-            // Loop through the data and create markers with rotation
-            data.forEach(bus => {
-                const { lat, lon, rotationAngle } = bus;
+        .then((data) => {
 
-                // Create a sample marker with rotation
-                const rotatedMarker = L.rotatedMarker([44.650690, -63.596537], {
-                    rotationAngle: 45, // Set the rotation angle in degrees
-                    icon: L.icon({
-                        iconUrl: './bus.png',
-                        iconSize: [32, 32]
-                    }),
-                }).addTo(map)
-            });
+            const filterData = data.entity.filter(
+                (bus) => bus.vehicle.trip.routeId >= 1 && bus.vehicle.trip.routeId <= 10
+            );
+
+
+
+            const geoJson = geoJSONFromData(filterData);
+
+            //updateMapWithCustomMarkers(geoJson);
+            console.log(filterData)
+            // data(bus => {
+            //     const { lat, lon, rotationAngle } = bus;
+
+            //     // Create a sample marker with rotation
+            //     const rotatedMarker = L.rotatedMarker([44.650690, -63.596537], {
+            //         rotationAngle: 45, // Set the rotation angle in degrees
+            //         icon: L.icon({
+            //             iconUrl: './bus.png',
+            //             iconSize: [32, 32]
+            //         }),
+            //     }).addTo(map)
+            // });
         });
+    function geoJSONFromData(data) {
+        return {
+            type: "FeatureCollection",
+            features: data.map((bus) => ({
+                type: "Feature",
+                properties: {
+                    bearing: bus.vehicle.position,
+                    route: bus.vehicle.trip.routeId,
+                    direction: bus.vehicle.trip.directionId,
+                },
+                geometry: {
+                    type: "Point",
+                    coordinates: [
+                        bus.vehicle.position.longitude,
+                        bus.vehicle.position.latitude
+                    ],
+
+                },
+
+            })),
+        };
+    }
 
 
 
     L.marker([44.650690, -63.596537]).addTo(map)
-        .bindPopup('This is a sample popup. You can put any html structure in this including extra bus data. You can also swap this icon out for a custom icon. A png file has been provided for you to use if you wish.')
+        .bindPopup('bus')
         .openPopup();
 
 
