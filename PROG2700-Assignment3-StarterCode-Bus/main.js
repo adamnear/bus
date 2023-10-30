4eb517ccd56d4c663f26954c4910e2af5ab94889
adamnear <nearing14@hotmail.com>
2023-10-30 12:33:17 -0300

    Busses refreshing working, and deleting old bus

--- PROG2700-Assignment3-StarterCode-Bus/main.js
+++ PROG2700-Assignment3-StarterCode-Bus/main.js
@@ -1,38 +1,21 @@
-// IIFE
+// IIFE (Immediately Invoked Function Expression)
 (() => {
-
-
-    //create map in leaflet and tie it to the div called 'theMap'
+    // Create a Leaflet map and set its initial view
     let map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
+    // Add a tile layer from OpenStreetMap with attribution
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    // Define a custom icon for markers
     let customIcon = L.icon({
         iconUrl: './bus.png',
         iconSize: [32, 32],
         iconAnchor: [16, 16],
     });
 
-    //Fetch live bus data
-
-
-    fetch('https://prog2700.onrender.com/hrmbuses')
-        .then(response => response.json())
-        .then((data) => {
-
-            const filterData = data.entity.filter(
-                (bus) => bus.vehicle.trip.routeId >= 1 && bus.vehicle.trip.routeId <= 10
-            );
-            console.log(filterData)
-
-            const geoJSON = geoJSONFromData(filterData);
-
-            updateMapWithCustomMarkers(geoJSON);
-        });
-
-
+    // Function to create a GeoJSON object from filtered data
     function geoJSONFromData(data) {
         return {
             type: "FeatureCollection",
@@ -49,54 +32,59 @@
                         bus.vehicle.position.longitude,
                         bus.vehicle.position.latitude
                     ],
-
                 },
-
             })),
         };
     }
 
+    // Function to update the map with custom markers
+    function updateMapWithCustomMarkers(geoJSON) {
+        L.geoJSON(geoJSON, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {
+                    icon: customIcon,
+                    rotationAngle: feature.properties.bearing,
+                });
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(
+                    `Route: ${feature.properties.route}<br>Direction: ${feature.properties.direction}`
+                );
+            },
+        }).addTo(map);
+    }
+
+    // Function to refresh the map data
     function refreshMap() {
-        // map.eachLayer((layer) => {
-        //     if (layer instanceof L.geoJson) {
-        //         map.removeLayer(layer);
-        //     }
-        // });
-        //Getting the live bus data
+        // Clear existing map data
+        map.eachLayer(function (layer) {
+            if (layer instanceof L.GeoJSON) {
+                map.removeLayer(layer);
+            }
+        });
+
+        // Fetch and process updated bus data
         fetch("https://prog2700.onrender.com/hrmbuses")
             .then((response) => response.json())
             .then((data) => {
-                // Filter and transform data
+                // Filter data for buses on routes 1-10
                 const filteredData = data.entity.filter(
                     (bus) =>
                         bus.vehicle.trip.routeId >= 1 && bus.vehicle.trip.routeId <= 10
                 );
-                const geoJson = geoJSONFromData(filteredData);
+                const geoJSON = geoJSONFromData(filteredData);
 
-                if (geoJsonLayer) {
-                    map.removeLayer(geoJsonLayer);
-                }
-                //GeoJSON being updated with the icon marker
-
-                geoJsonLayer = L.geoJSON(geoJSON, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.marker(latlng, {
-                            icon: customIcon,
-                            rotationAngle: feature.properties.bearing,
-                        });
-                    },
-                    onEachFeature: function (feature, layer) {
-                        layer.bindPopup(
-                            `Route: ${feature.properties.route}<br>Direction: ${feature.properties.direction}`
-                        );
-                    },
-                }).addTo(map);
-            }
+                // Update the map with custom markers
+                updateMapWithCustomMarkers(geoJSON);
+            })
+            .catch((error) => {
+                console.error("Error fetching and processing data:", error);
             });
-}
+    }
 
-
+    // Set an interval to refresh the map every 3 seconds (3000 milliseconds)
     setInterval(refreshMap, 3000);
 
-refreshMap();
-}) ();
+    // Initial map data refresh
+    refreshMap();
+})();
